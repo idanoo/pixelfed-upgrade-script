46a421b29807b80f33a508ab5430894b50d4f325
Daniel Mason <daniel@m2.nz>
2019-06-02 17:16:31 +1200

    Fix paths

--- update.sh
+++ update.sh
@@ -8,12 +8,11 @@ WEB_USER=www-data
 # DO NOT EDIT BELOW THIS LINE #
 set +x
 
-cd $WEB_ROOT
-su - $WEB_USER -s /bin/bash -c 'git pull'
-su - $WEB_USER -s /bin/bash -c 'composer install --no-ansi --no-interaction --no-progress --no-scripts --optimize-autoloader'
-su - $WEB_USER -s /bin/bash -c 'php artisan config:cache'
-su - $WEB_USER -s /bin/bash -c 'php artisan route:cache'
-su - $WEB_USER -s /bin/bash -c 'php artisan migrate --force'
-su - $WEB_USER -s /bin/bash -c 'php artisan horizon:purge'
-su - $WEB_USER -s /bin/bash -c 'php artisan storage:link'
+su - $WEB_USER -s /bin/bash -c "cd $WEB_ROOT && git pull"
+su - $WEB_USER -s /bin/bash -c "cd $WEB_ROOT && composer install --no-ansi --no-interaction --no-progress --no-scripts --optimize-autoloader"
+su - $WEB_USER -s /bin/bash -c "cd $WEB_ROOT && php artisan config:cache"
+su - $WEB_USER -s /bin/bash -c "cd $WEB_ROOT && php artisan route:cache"
+su - $WEB_USER -s /bin/bash -c "cd $WEB_ROOT && php artisan migrate --force"
+su - $WEB_USER -s /bin/bash -c "cd $WEB_ROOT && php artisan horizon:purge"
+su - $WEB_USER -s /bin/bash -c "cd $WEB_ROOT && php artisan storage:link"
 
