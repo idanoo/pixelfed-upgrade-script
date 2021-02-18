5fa2a4ec056c9d1f86682c5680f31bcd26c00c7e
Daniel Mason <daniel@m2.nz>
2021-02-18 17:52:32 +1300

    Update repo

--- update.sh
+++ update.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 # CONFIGURATION #
-
 WEB_ROOT=/var/www/pixelfed
 WEB_USER=www-data
+# END CONFIGURATION #
 
-# DO NOT EDIT BELOW THIS LINE #
 set +x
 
 su - $WEB_USER -s /bin/bash -c "cd $WEB_ROOT && git pull"
@@ -13,6 +12,9 @@ su - $WEB_USER -s /bin/bash -c "cd $WEB_ROOT && composer install --no-ansi --no-
 su - $WEB_USER -s /bin/bash -c "cd $WEB_ROOT && php artisan config:cache"
 su - $WEB_USER -s /bin/bash -c "cd $WEB_ROOT && php artisan route:cache"
 su - $WEB_USER -s /bin/bash -c "cd $WEB_ROOT && php artisan migrate --force"
+
+# Make sure we restart workers on new code too
 sudo supervisorctl restart horizon
+
 #su - $WEB_USER -s /bin/bash -c "cd $WEB_ROOT && php artisan horizon:purge"
 #su - $WEB_USER -s /bin/bash -c "cd $WEB_ROOT && php artisan storage:link"
