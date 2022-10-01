b3fa4089640adca6ae9ddebb348d64c683133224
idanoo <daniel@m2.nz>
2022-10-01 20:05:22 +1300

    No-dev and dump autoload

--- update.sh
+++ update.sh
@@ -8,7 +8,8 @@ WEB_USER=www-data
 set +x
 
 su - $WEB_USER -s /bin/bash -c "cd $WEB_ROOT && git pull"
-su - $WEB_USER -s /bin/bash -c "cd $WEB_ROOT && composer install --no-ansi --no-interaction --no-progress --no-scripts --optimize-autoloader"
+su - $WEB_USER -s /bin/bash -c "cd $WEB_ROOT && composer install --no-ansi --no-dev --no-interaction --no-progress --no-scripts --optimize-autoloader"
+su - $WEB_USER -s /bin/bash -c "cd $WEB_ROOT && composer dump-autoload --optimize"
 su - $WEB_USER -s /bin/bash -c "cd $WEB_ROOT && php artisan config:cache"
 su - $WEB_USER -s /bin/bash -c "cd $WEB_ROOT && php artisan route:cache"
 su - $WEB_USER -s /bin/bash -c "cd $WEB_ROOT && php artisan migrate --force"
